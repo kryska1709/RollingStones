78d0724007efd3acd9edf3a1598e8fe9e79704c9
Your Name <you@example.com>
2025-06-03 11:50:38 +0300

    test2

--- app/src/main/java/com/example/rollingstones/view/HomeView.kt
+++ app/src/main/java/com/example/rollingstones/view/HomeView.kt
@@ -49,7 +49,7 @@ fun HomeView(
             .padding(16.dp)
     ) {
         Text(
-            text = "Самая главная",
+            text = "главная",
             color = MainColor,
             fontSize = 28.sp,
             fontWeight = FontWeight.Bold,
